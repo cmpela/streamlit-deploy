6fc9699cd6e274b3b77746cd55d4173dfdd7f77b
Conrado <cmpela@gmail.com>
2023-06-04 16:14:36 -0300

    traduzindo aplicação para o inglês

--- suicide_pred_model_hf.py
+++ suicide_pred_model_hf.py
@@ -4,11 +4,11 @@ import torch
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
 # Configurando a página
-st.set_page_config(page_title="Predição de suícidio", page_icon=None, layout="centered", menu_items=None)
-st.title('Essa pessoa precisa de ajuda?')
-st.markdown('<div style="text-align: justify;">Com a tecnologia e redes sociais, informações pessoais são compartilhadas online. Modelo de AI pode analisar padrões linguísticos/comportamentais presentes nas postagens para prever risco de suicídio. A abordagem permite detecção precoce de sinais de alerta para intervenção rápida e prevenção da tragédia. Porém, não deve substituir avaliação clínica, mas ser uma ferramenta complementar. Aumenta eficácia dos esforços de prevenção do suicídio.</div>', unsafe_allow_html=True)
+st.set_page_config(page_title="Suicide Prediction", page_icon=None, layout="centered", menu_items=None)
+st.title('Knows a person who need help?')
+st.markdown('<div style="text-align: justify;">With technology and social media, personal information is shared online. AI model can analyze linguistic/behavioral patterns present in posts to predict suicide risk. The approach allows early detection of warning signs for rapid intervention and prevention of tragedy. However, it should not replace clinical assessment but be a complementary tool. It increases effectiveness of suicide prevention efforts.</div>', unsafe_allow_html=True)
 st.divider()
-st.markdown('<div style="text-align: justify;">Para usar a ferramenta, escreva abaixo o texto para que o modelo possa retornar uma classifiação indicando se a pessoa precisa ou não de ajuda especializada:</div>', unsafe_allow_html=True)
+st.markdown('<div style="text-align: justify;">To use the tool, type text below so that the model can return a rating indicating whether or not the person needs specialist help:</div>', unsafe_allow_html=True)
 
 
 #Inicializando o modelo pré-treainado
@@ -17,7 +17,7 @@ model_hf = AutoModelForSequenceClassification.from_pretrained('gooohjy/suicidal-
 
     
 #Configurando a interação do usuário    
-input_text = st.text_input('Insira o texto aqui', help='Insira um texto, ou trecho dele para avaliar se existe indícios suícida')
+input_text = st.text_input('Insert the text here', help='Enter a text, or an extract from it, to assess whether there is evidence of suicide.')
 
 
 #Configurando a predição
@@ -28,8 +28,8 @@ result = int(torch.argmax(result.logits))
      
 # "Rodando" a predição a partir do texto informado
 
-if st.button('Obter resultado', help='Clique em "Obter resultado" para gerar uma avaliação do modelo de AI'):
+if st.button('Get result', help='Click on "Get Result" to generate an evaluation of the IA model'):
    if (result == 1):
-        st.warning('Essa pessoa potencialmente precisa de ajuda', icon="⚠️")
+        st.warning('This person potentially needs help', icon="⚠️")
    else:
-        st.success('Essa pessoa não apresenta indicios de risco', icon = "✅")
+        st.success('This person presents no indications of risk', icon = "✅")
